3551c2f69ff7e52ad8c1cddcddcca62d02438940
longsiyang <longsiyang@kuaishou.com>
2019-12-10 20:53:14 +0800

    init vue proj

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <p id="title">工具集</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+#app {
+  text-align: center;
+}
+
+#title {
+  font-size: 50px;
+}
+</style>
